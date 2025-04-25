703638523e90b5ae1c8e5ac1389e1b1e21ae0148
John Carrington <kanie.w@mail.ru>
2025-04-25 11:41:24 +0500

    Add audio endpoint

--- src/api/endpoints.py
+++ src/api/endpoints.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
-from src.api.routes.graph_text_input import router as graph_router
+from src.api.routes.graph_text_input import router as graph_input_router
+from src.api.routes.graph_audio_input import router as graph_audio_router
 
 router = APIRouter()
+routers = [graph_audio_router, graph_input_router]
 
-router.include_router(router=graph_router)
+for route in routers:
+    router.include_router(router=route)
